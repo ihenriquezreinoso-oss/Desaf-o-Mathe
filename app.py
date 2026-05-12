d0ec72f915e46b68096ef291285f9f98d4f6816e
ihenriquezreinoso-oss <ihenriquezreinoso@gmail.com>
2026-05-11 21:56:22 -0400

    Update app.py

--- app.py
+++ app.py
@@ -99,13 +99,13 @@ m3.metric(
 st.divider()
 st.subheader("🔗 Cadena de Medición: Del Aire al Dato")
 st.info("")
-st.write()
-st.write() 1. **Impacto:** Una partícula en el aire "choca" con nuestro sensor (Nanocantilever).
+st.write("""
+1. **Impacto:** Una partícula en el aire "choca" con nuestro sensor (Nanocantilever).
 2. **Movimiento:** El sensor vibra como un trampolín minúsculo (esto genera la onda graficada).
 3. **Conversión:** Un material especial (piezoeléctrico) transforma ese movimiento en electricidad (Diferencia de potencial "Voltaje").
 4. **Resultado:** La computadora aplica una operación matemática; **Transformada de Fourier**. Esta "desarma" la señal de voltaje para 
 encontrar su frecuencia exacta. Si la frecuencia bajó, significa que el sensor ahora es más pesado: hemos detectado una partícula. 
-
+""")
 
 st.caption("A partir de esta onda senoidal, se extrae la frecuencia característica mediante la Transformada de Fourier.")
 # Pie de página
